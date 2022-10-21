ed9d9cfde3e2937899f6a7c1a5b3f4e8173b02b5
Stepan Anokhin <stepan.anokhin@gmail.com>
2022-10-21 20:43:42 +0700

    Implement canary release

---
+++ scripts/canary-release.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -x
+
+FEATURE_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+COMMIT="$(git rev-parse --short HEAD)"
+
+# Create branch-specific version
+npx lerna version --no-changelog --conventional-commits --conventional-prerelease --no-push --preid "$FEATURE_BRANCH-$COMMIT" --yes
+
+# Publish canary release
+npx lerna publish from-git --dist-tag "$FEATURE_BRANCH" --yes
+
+git reset --hard HEAD~1
